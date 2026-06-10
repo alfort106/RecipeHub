86d7ede5f4fc56c312f9e391884ff4ff5270f305
alfort106 <alfort106@gmail.com>
2026-06-10 15:46:34 +0900

    Initial Commit

---
+++ RecipeHub/RecipeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeHubApp.swift
+//  RecipeHub
+//
+//  Created by arakawa hinata on 2026/06/10.
+//
+
+import SwiftUI
+
+@main
+struct RecipeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
